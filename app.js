789aa49587c4cfb68f38adbe2da96ae6a9bbdd80
Artem <75565567+Artem589@users.noreply.github.com>
2021-12-12 08:30:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
+
 const input = document.querySelector(".input-get");
 const btn = document.querySelector(".btn-get");
-const result = document.querySelector(".result");
+const resultInner = document.querySelector(".result-inner");
 
 btn.addEventListener("click", () => {
   //link generation
-  const firstName = input.value;
+  const firstName = input.value
   const serverGender = "https://api.genderize.io";
   const serverNation = "https://api.nationalize.io";
   const urlGender = `${serverGender}?name=${firstName}`;
   const urlNation = `${serverNation}?name=${firstName}`;
-
+ 
   //gender value
+ 
   fetch(urlGender)
     .then(response => response.json())
-    .then(res => result.textContent = `${firstName} is ${res.gender}`)
-
+    .then(result => {
+       if(result.gender === null) {
+        resultInner.textContent = `${firstName} is not found`;
+      } else {
+        resultInner.textContent = `${firstName} is ${result.gender}`;
+      }
+    })
+ 
   // In which country is it more common
   fetch(urlNation)
   .then(response => response.json())
-  .then(res => (res.country).reduce((acc,curr) => acc.probability > curr.probability ? acc : curr))
-  .then(res => result.textContent = `${result.textContent} from country: ${res.country_id}`)
+  .then(result => (result.country).reduce((acc,curr) => acc.probability > curr.probability ? acc : curr))
+  .then(result => resultInner.textContent = `${resultInner.textContent} from country: ${result.country_id}`)
+  
 });
 
 
